d55cc2fe024e380332c105a49d3908f0958bf88e
skarolk <skarolkiewicz@gmail.com>
2019-03-18 16:13:07 -0400

    last commit before moving to frontend

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 puts "creating movies"
 
@@ -116,5 +111,3 @@ create_movies()
 # Rating.create(user_id: 15, liked: false, movie_id: 1)
 
 puts "movies created"
-
-# puts "Reminder: this is only test data"
