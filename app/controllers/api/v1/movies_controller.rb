c45766d8fcf252f6d92f303ee5af714b6b25699b
skarolk <skarolkiewicz@gmail.com>
2019-03-18 13:05:34 -0400

    added fast loading random query

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -7,8 +7,12 @@ class Api::V1::MoviesController < ApplicationController
 
     def index
         #page/limit on queries coming from frontend
-        offset = params[:page].to_i*params[:limit].to_i - params[:limit].to_i + 1
-        limit = params[:page].to_i*params[:limit].to_i
+        # offset = params[:page].to_i*params[:limit].to_i - params[:limit].to_i + 1
+        # limit = params[:page].to_i*params[:limit].to_i
+
+        #random offset
+        offset = Random.rand(Movie.all.length - 10) - 1
+        limit = offset + 10
 
         #serving up batch of results requested
         #200 results serve up quickly
@@ -16,6 +20,12 @@ class Api::V1::MoviesController < ApplicationController
         @movies = Movie.find_in_batches(start: offset, finish: limit)
 
         render json: @movies
+    end
+
+    def show
+        @movie = Movie.find_by(id: params[:id])
+        
+        render json: @movie
     end
 
     private
